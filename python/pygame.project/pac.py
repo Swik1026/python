412f2dd848080bfe3a6f813f933bf80db09c63c4
swik1026 <hsjkor82@gmail.com>
2021-08-22 21:58:31 +0900

    210822 complete

--- python/pygame.project/pac.py
+++ python/pygame.project/pac.py
@@ -1,4 +1,13 @@
-lst = ["가", "나", "다"]
+balls = [1, 2, 3, 4]
+weapons = [11, 22, 3, 44]
 
-for lst_idx, lst_val in enumerate(lst):
-    print(lst_idx, lst_val)
+for ball_idx, ball_val in enumerate(balls):
+    print("ball", ball_val)
+    for weapon_idx, weapon_val in enumerate(weapons):
+        print("weapons :", weapon_val)
+        if ball_val == weapon_val:
+            print("공과 무기가 충돌")
+            break
+    else:
+        continue
+    break
